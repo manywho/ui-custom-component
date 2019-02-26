7424f50eaa8054b1ba6659d8d1d2ae2d1c8a23fe
Bates <Matthew_Bates@Dell.com>
2019-02-26 12:58:09 +0000

    Removed dependency on bootstrap for debug HoC

--- src/utils/debug.tsx
+++ src/utils/debug.tsx
@@ -3,17 +3,11 @@ import { IComponentModel, IComponentProps } from '../interfaces';
 import './debug.css';
 
 const getModal = (model: IComponentModel, data: any, type: string) => (
-    <div className="modal fade" id={`${model.id}-${type}`} tabIndex={-1} role="dialog" style={{ position: 'fixed' }}>
-        <div className="modal-dialog modal-lg" role="document">
-            <div className="modal-content">
-                <div className="modal-header">
-                    <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
-                <h4 className="modal-title">{type} - {model.developerName}</h4>
-                </div>
-                <div className="modal-body">
-                    <pre>{JSON.stringify(data, null, 4)}</pre>
-                </div>
-            </div>
+    <div className="modal-debug" id={`${model.id}-${type}`} tabIndex={-1} role="dialog">
+        <div>
+            <a href="#" title="Close" className="modal-close">Close</a>
+            <h4>{type} - {model.developerName}</h4>
+            <pre>{JSON.stringify(data, null, 4)}</pre>
         </div>
     </div>
 );
@@ -27,8 +21,8 @@ export const debugComponent = (
             <div>
                 <span>{props.model.developerName}</span>
                 <span>{props.model.id}</span>
-                <button className="btn btn-link btn-sm" data-toggle="modal" data-target={`#${props.model.id}-model`}>Model</button>
-                <button className="btn btn-link btn-sm" data-toggle="modal" data-target={`#${props.model.id}-state`}>State</button>
+                <a href={`#${props.model.id}-model`}>Model</a>
+                <a href={`#${props.model.id}-state`}>State</a>
             </div>
             {React.createElement(Component, props)}
             {getModal(props.model, props.model, 'model')}
